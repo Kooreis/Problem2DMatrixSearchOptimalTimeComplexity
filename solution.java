fda00754f8f38b1b74e0cf55e50dd4a400d43532
Kooreis <kooreis.official@gmail.com>
2024-06-23 16:46:06 -0500

    This part of the script uses nested for loops to fill the matrix with elements entered by the user.

--- solution.java
+++ solution.java
@@ -1,11 +1,5 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter number of rows:");
-        int rows = scanner.nextInt();
-        System.out.println("Enter number of columns:");
-        int columns = scanner.nextInt();
-        int[][] matrix = new int[rows][columns];
-        System.out.println("Enter matrix elements:");
+for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
